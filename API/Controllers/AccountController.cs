65a288f6a2910daf29688e21daf665a1e9bbf857
Aditya Kodi <ramadityakodi@gmail.com>
2026-05-11 15:29:11 +0530

    End Of Section $

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,7 +1,10 @@
 using System.Security.Cryptography;
+using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
+using API.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,10 +12,10 @@ using Microsoft.EntityFrameworkCore;
 namespace API.Controllers
 {
  
-    public class AccountController (AppDbContext context) : BaseApiController
+    public class AccountController (AppDbContext context, ITokenService tokenService) : BaseApiController
     {
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(RegisterDto dto)
+        public async Task<ActionResult<UserDto>> Register(RegisterDto dto)
         {
             if(await EmailExists(dto.Email)) return BadRequest("Email is already taken");
             using var hmac = new HMACSHA512();
@@ -25,8 +28,31 @@ namespace API.Controllers
             };
             context.Users.Add(user);
             await context.SaveChangesAsync();
-            return user;
+
+             return user.ToDto(tokenService);
         }
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await context.Users.SingleOrDefaultAsync(user => user.Email == loginDto.Email);
+             
+            if(user == null) return Unauthorized();
+
+            var hmac = new HMACSHA512(user.PasswordSalt);
+            var passwordhash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for(int i = 0 ; i < passwordhash.Length ; i++)
+            {
+                if(passwordhash[i] != user.PasswordHash[i]) return Unauthorized();
+
+            } 
+
+            return user.ToDto(tokenService);
+
+        }
+
+
+
 
         private async Task<bool> EmailExists(string email)
         {
