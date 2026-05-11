cce6cfec3c7c1aa744fcf81afce88f5f59878938
Aditya Kodi <ramadityakodi@gmail.com>
2026-05-11 10:25:41 +0530

    UI Changes

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+using System.Security.Cryptography;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+ 
+    public class AccountController (AppDbContext context) : BaseApiController
+    {
+        [HttpPost("register")]
+        public async Task<ActionResult<AppUser>> Register(RegisterDto dto)
+        {
+            if(await EmailExists(dto.Email)) return BadRequest("Email is already taken");
+            using var hmac = new HMACSHA512();
+            var user = new AppUser
+            {
+                DisplayName = dto.DisplayName,
+                Email = dto.Email,
+                PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(dto.Password)),
+                PasswordSalt = hmac.Key
+            };
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+            return user;
+        }
+
+        private async Task<bool> EmailExists(string email)
+        {
+            return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower());
+        }
+
+    }
+}
---
+++ client/src/app/app.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+   private http = inject(HttpClient);
+  title = 'Dating App';
+  protected members = signal<any>([]);
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/members').subscribe({
+      next: response => {this.members.set(response)},
+      error: error=> console.log(error),
+      complete:()=>console.log("API Call Is completed !!")
+    });
+  }
+}
